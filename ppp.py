1c4cc2bcbdf7044893822bfb81e8b85eebd09310
Andrew Kong <113311235+xuechenz@users.noreply.github.com>
2025-07-16 10:42:19 -0400

    Update ppp.py

--- ppp.py
+++ ppp.py
@@ -1,18 +1,1 @@
-vmax = max(abs(matrix.min()), abs(matrix.max()))
-
-fig = go.Figure(
-    go.Heatmap(
-        z=matrix,
-        x=x_vals,
-        y=y_vals,
-        colorscale=[
-            [0.0, 'red'],    
-            [0.5, 'white'],  
-            [1.0, 'green']   
-        ],
-        zmin=-vmax,
-        zmax=+vmax,
-        zmid=0,
-        hovertemplate= "Strike: %{y:.2f}<br>Tenor: %{x}m<br>Vega: %{z:.4f}<extra></extra>"
-    )
-)
+vmax = max(abs(v) for row in matrix for v in row)
