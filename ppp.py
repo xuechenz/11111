1b21a9db0437b6653dcb78cfa75ddc6279ff7d1a
Andrew Kong <113311235+xuechenz@users.noreply.github.com>
2025-07-17 01:17:13 -0400

    Update ppp.py

--- ppp.py
+++ ppp.py
@@ -1,3 +1,1 @@
-today = pd.Timestamp.today()  
-yesterday_bd = (today - pd.tseries.offsets.BDay(1)).date()
-today_str = yesterday_bd.strftime("%Y-%m-%d")
+html.Div(id="vega-extra-container", style={"marginTop":"20px"}),
