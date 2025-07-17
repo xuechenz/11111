517922b86225b4ea5efc8b905bf6ec5d640cd618
Andrew Kong <113311235+xuechenz@users.noreply.github.com>
2025-07-17 16:23:46 -0400

    Update email.py

--- email.py
+++ email.py
@@ -1,16 +1,23 @@
 navbar = dbc.Navbar(
     dbc.Container(
-        [
-            html.Img(src="/assets/TD_Securities_logo.svg", height="40px"),
-            dbc.NavbarBrand(
-                "Vega Map Generator",
-                className="fw-bold text-dark"
-            ),
-        ],
+        dbc.NavbarBrand(
+            [
+                html.Img(
+                    src="/assets/TD_Securities_logo.svg",
+                    height="40px",
+                    className="me-1"   
+                ),
+                html.Span(
+                    "Vega Map Generator",
+                    className="fw-bold text-dark"
+                ),
+            ],
+            className="d-flex align-items-center px-0"
+        ),
         fluid=True,
-        className="px-0"
+        className="px-0" 
     ),
     className="bg-white shadow-sm",
     sticky="top",
-    style={"padding":"20px 0"}
+    style={"padding":"20px 0"} 
 )
